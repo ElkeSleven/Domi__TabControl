020e73ade9432cf9a0633ff4ebabe25cb006b2ad
Elke Sleven <myprogrammersmail@gmail.com>
2022-06-07 23:05:46 +0200

    Domi__TabControl
    XAML van TabControl:

--- Domi__TabControl/MainWindow.xaml.cs
+++ Domi__TabControl/MainWindow.xaml.cs
@@ -13,6 +13,35 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+//  Bekijk de Xaml
+/*// TabControl
+ * Tabbladen (zoals in webbrowsers).
+ * Handig om veel info op 1 Window te krijgen en te verdelen.
+ * Wisselen tussen tabbladen: Ctrl+Tab
+ * Properties:
+ *   - Items: bevat de tabbladen
+ *   - TabStripPlacement: waar komen tabknoppen (Left, Right, Top, Bottom).
+ * Methods:
+ *   - Focus zetten op 1ste tabblad: tabMijnTabblad.SelectedIndex = 0; // index -1: geen tabblad
+ * XAML van TabControl:
+ *   - TabControl bestaat uit TabItems (tabbladen) met een Header (titel).
+ *   - In elk TabItem kan je een Grid, DockPanel, StackPanel,... zetten.
+ *   - HorizontalAlignment="Stretch" en VerticalAlignment="Stretch“
+ *   - Tabblad zowel horizontaal als verticaal uitrekken bij resizen van Window
+ *   - Binnen de Header kan je ook Images of andere controls zetten gebruik hiervoor een StackPanel
+ */
+
+/*// StatusBar
+ * StatusBar = statusbalk ⇒ status weergeven van toepassing
+ * Kan je onderverdelen in verschillende controls
+ * Staat meestal onderaan Window
+ * 
+ */
+
+
+
+
+
 namespace Domi__TabControl
 {
     /// <summary>
